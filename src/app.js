f4f6007405ec404042e5703a014e88514b9b0dcd
forthekellofit <67642963+forthekellofit@users.noreply.github.com>
2020-07-28 12:05:38 -0400

    remove console logs

--- src/app.js
+++ src/app.js
@@ -40,8 +40,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
-
   let temperature = document.querySelector("#current-temp");
   temperature.innerHTML = Math.round(response.data.main.temp);
   let h1 = document.querySelector("h1");
@@ -58,5 +56,4 @@ function displayTemperature(response) {
 let apiKey = "855dcf2816c3df70674db30fa73e361c";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
